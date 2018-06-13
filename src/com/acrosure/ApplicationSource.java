20897f3380e0d81667cc039d6874acc289f661f7
Kohpai <whcwhc78@gmail.com>
2018-06-13 18:00:25 +0700

    Type migration from String to Enum

---
+++ src/com/acrosure/ApplicationSource.java
@@ -0,0 +1,17 @@
+package com.acrosure;
+
+public enum ApplicationSource {
+    PARTNER("PARTNER"),
+    CUSTOMER("CUSTOMER");
+
+    private final String source;
+
+    ApplicationSource(String source) {
+        this.source = source;
+    }
+
+    @Override
+    public String toString() {
+        return source;
+    }
+}
