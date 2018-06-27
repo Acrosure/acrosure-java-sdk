7851b96c0de68b2d11890256aa9ff3a36136ab3f
Kohpai <whcwhc78@gmail.com>
2018-06-27 16:55:41 +0700

    switch from org.apache.httpcomponents to com.squareup.okhttp3

--- src/main/java/com/acrosure/HttpClient.java
+++ src/main/java/com/acrosure/HttpClient.java
@@ -1,88 +1,49 @@
 package com.acrosure;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.StatusLine;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpRequestBase;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
+import okhttp3.*;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 
 public class HttpClient implements IHttpClient {
     private final String token;
     private final String HOST;
-    private final CloseableHttpClient httpClient;
+    private final OkHttpClient httpClient;
 
     HttpClient(String token) {
         this.token = token;
-        httpClient = HttpClients.createDefault();
+        httpClient = new OkHttpClient.Builder().build();
         HOST = "https://api.phantompage.com";
     }
 
     @Override
     public Object call(String method, String methodGroup, JSONObject param) throws IOException, AcrosureException {
         JSONObject apiResponse;
-        HttpRequestBase httpRequest = buildHttpRequest(method, methodGroup, param);
+        Request request = buildHttpRequest(method, methodGroup, param);
+        Call call = httpClient.newCall(request);
+        Response response = call.execute();
+        int statusCode = response.code();
+        apiResponse = new JSONObject(response.body().string());
 
-        try (CloseableHttpResponse httpResponse = httpClient.execute(httpRequest)) {
-            int statusCode;
-            StatusLine statusLine = httpResponse.getStatusLine();
-            HttpEntity recEntity = httpResponse.getEntity();
-            BufferedReader content = new BufferedReader(new InputStreamReader(
-                    recEntity.getContent(),
-                    StandardCharsets.UTF_8));
 
-            apiResponse = parseJson(content);
-            statusCode = statusLine.getStatusCode();
-            EntityUtils.consume(recEntity);
-
-            if (statusCode != 200) {
-                if (apiResponse == null)
-                    throw new AcrosureException(statusLine.getReasonPhrase(), statusCode);
-                else
-                    throw new AcrosureException((String) apiResponse.getString("message"), statusCode);
-            }
+        if (statusCode != 200) {
+            if (apiResponse == null)
+                throw new AcrosureException(response.message(), statusCode);
+            else
+                throw new AcrosureException(apiResponse.getString("message"), statusCode);
         }
 
         return apiResponse.get("data");
     }
 
-    private HttpRequestBase buildHttpRequest(String method, String methodGroup, JSONObject param) {
-        StringEntity entity = new StringEntity(
-                param.toString(),
-                ContentType.create("application/json", "UTF-8"));
-        HttpPost httpPost = new HttpPost(HOST + "/" + methodGroup + "/" + method);
+    private Request buildHttpRequest(String method, String methodGroup, JSONObject param) {
+        RequestBody body = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), param.toString());
 
-        httpPost.addHeader("Authorization", "Bearer " + this.token);
-        httpPost.setEntity(entity);
-
-        return httpPost;
-    }
-
-    private JSONObject parseJson(BufferedReader content) throws IOException {
-        StringBuilder jsonString = new StringBuilder("");
-
-        while (content.ready())
-            jsonString.append(content.readLine());
-
-        JSONObject jsonObject = new JSONObject(jsonString.toString());
-
-        return jsonObject;
-    }
-
-    private void printContent(BufferedReader content) throws IOException {
-        while (content.ready())
-            System.out.print(content.readLine());
-
-        System.out.println();
+        return new Request.Builder()
+                .url(HOST + "/" + methodGroup + "/" + method)
+                .addHeader("Authorization", "Bearer " + this.token)
+                .post(body).build();
     }
 }
