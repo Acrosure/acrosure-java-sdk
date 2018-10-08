3ba0948e7ac550a53d71c486fb2454e3f42f6a76
Srikote Naewchampa <whcwhc78@gmail.com>
2018-10-08 23:17:44 +0700

    use JUnit to write integration test

--- src/test/java/CARIntegrationTest.java
+++ src/test/java/CARIntegrationTest.java
@@ -1,93 +1,210 @@
+import static org.junit.jupiter.api.Assertions.*;
+
 import com.acrosure.*;
-import com.acrosure.Application;
 import com.acrosure.Package;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
-import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
 
-public class CARIntegrationTest {
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class CARIntegrationTest {
+    private Acrosure client;
+    private ObjectMapper mapper;
+    private File basicDataFile;
+    private File additionalDataFile;
+    private Application application;
+    private Package[] packages;
 
-    public static void main(String[] args) {
-        ObjectMapper mapper = new ObjectMapper();
+    @BeforeAll
+    void init() {
+        client = new Acrosure("tokn_sample_secret", "http://localhost:8000");
+        mapper = new ObjectMapper();
         mapper.setPropertyNamingStrategy(new PropertyNamingStrategy.SnakeCaseStrategy());
 
-        File basicDataFile = new File("/home/kohpai/IdeaProjects/acrosure-java-sdk/src/test/java/basic_data.json");
-        File additionalDataFile = new File("/home/kohpai/IdeaProjects/acrosure-java-sdk/src/test/java/additional_data.json");
-        File policiesFile = new File("/home/kohpai/IdeaProjects/acrosure-java-sdk/src/test/java/policies.json");
-
-        Acrosure client = new Acrosure("tokn_sample_secret", "https://localhost:8000");
-        Acrosure adminClient = new Acrosure("sandbox_tokn_ghlKOqtR5iM1bQXh", "http://localhost:8000");
-
         try {
-            ObjectNode basicData = (ObjectNode) mapper.readTree(basicDataFile);
-            ObjectNode additionalData = (ObjectNode) mapper.readTree(additionalDataFile);
-            ArrayNode arrayPolicies = (ArrayNode) mapper.readTree(policiesFile);
-
-            Policy[] requestPolicies = new Policy[arrayPolicies.size()];
-            for (int i = 0; i < arrayPolicies.size(); ++i) {
-                requestPolicies[i] = mapper.treeToValue(arrayPolicies.get(i), Policy.class);
-            }
-
-            ApplicationCreateForm applicationCreateForm = new ApplicationCreateForm();
-            applicationCreateForm.setProductId("prod_contractor");
-            applicationCreateForm.setBasicData(basicData);
-
-            Application app = client.applications().create(applicationCreateForm);
-            System.out.println("After creating application...");
-            System.out.println(app);
-            System.out.println(app.getStatus() == ApplicationStatus.PACKAGE_REQUIRED);
-
-            Package[] packages = client.applications().getPackages(app.getId());
-            System.out.println("\nAfter getting packages...");
-            System.out.println(Arrays.toString(packages));
-
-            client.applications().selectPackage(app, packages[0].getPackageCode());
-            System.out.println("\nAfter selecting a package...");
-            System.out.println(app);
-            System.out.println(app.getStatus() == ApplicationStatus.DATA_REQUIRED);
-
-            app.setAdditionalData(additionalData);
-            client.applications().update(app);
-            System.out.println("\nAfter updating application...");
-            System.out.println(app);
-            System.out.println(app.getStatus() == ApplicationStatus.DATA_REQUIRED);
-
-            app.getAdditionalData().put("project_name", "โครงการ อโครบิวดิ้ง");
-            ObjectNode projectSite = app.getAdditionalData().putObject("project_site");
-            projectSite.put("subdistrict", "จอมพล");
-            projectSite.put("district", "จตุจักร");
-            projectSite.put("province", "กรุงเทพมหานคร");
-            client.applications().update(app);
-            System.out.println("\nAfter updating application...");
-            System.out.println(app);
-            System.out.println(app.getStatus() == ApplicationStatus.READY);
-
-            Application app2 = client.applications().get(app.getId());
-            System.out.println("\nAfter getting application...");
-            System.out.println(app2);
-
-            client.applications().submit(app);
-            System.out.println("\nAfter submitting application...");
-            System.out.println(app);
-            System.out.println(app.getStatus() == ApplicationStatus.SUBMITTED);
-
-            Policy[] policies = adminClient.applications().approve(app, requestPolicies);
-            System.out.println("\nAfter approving application...");
-            System.out.println(Arrays.toString(policies));
-            System.out.println(app);
-            System.out.println(app.getStatus() == ApplicationStatus.COMPLETED);
-
+            application = client.application().create("prod_contractor");
         } catch (IOException e) {
-            System.out.println(e.getMessage());
             e.printStackTrace();
+            fail();
         } catch (AcrosureException e) {
             System.out.println(e.getMessage() + ", " + e.getStatusCode());
             e.printStackTrace();
+            fail();
+        }
+
+        basicDataFile = new File("/home/kohpai/IdeaProjects/acrosure-java-sdk/src/test/java/basic_data.json");
+        additionalDataFile = new File("/home/kohpai/IdeaProjects/acrosure-java-sdk/src/test/java/additional_data.json");
+    }
+
+    @Test
+    void Create_WithOnlyProductId_ReturnsInitialApplication() {
+        try {
+            application = client.application().create("prod_contractor");
+            assertSame(application.getStatus(), ApplicationStatus.INITIAL);
+        } catch (IOException e) {
+            e.printStackTrace();
+            fail();
+        } catch (AcrosureException e) {
+            System.out.println(e.getMessage() + ", " + e.getStatusCode());
+            e.printStackTrace();
+            fail();
+        }
+    }
+
+    @Test
+    void Create_WithCompleteBasicData_AdvancesStatusToPackageRequired() {
+        ApplicationCreateForm applicationCreateForm = new ApplicationCreateForm();
+        applicationCreateForm.setProductId("prod_contractor");
+
+        try {
+            ObjectNode basicData = (ObjectNode) mapper.readTree(basicDataFile);
+            applicationCreateForm.setBasicData(basicData);
+            application = client.application().create(applicationCreateForm);
+
+            assertSame(application.getStatus(), ApplicationStatus.PACKAGE_REQUIRED);
+        } catch (IOException e) {
+            e.printStackTrace();
+            fail();
+        } catch (AcrosureException e) {
+            System.out.println(e.getMessage() + ", " + e.getStatusCode());
+            e.printStackTrace();
+            fail();
+        }
+    }
+
+    @Test
+    void Get_ByDefault_ReturnsApplication() {
+        try {
+            String id = application.getId();
+            application = client.application().get(id);
+
+            assertEquals(application.getId(), id);
+        } catch (IOException e) {
+            e.printStackTrace();
+            fail();
+        } catch (AcrosureException e) {
+            System.out.println(e.getMessage() + ", " + e.getStatusCode());
+            e.printStackTrace();
+            fail();
+        }
+    }
+
+    @Test
+    void GetPackages_ByDefault_ReturnsPackages() {
+        try {
+            if (application.getStatus() == ApplicationStatus.INITIAL) {
+                ApplicationCreateForm applicationCreateForm = new ApplicationCreateForm();
+                applicationCreateForm.setProductId("prod_contractor");
+                ObjectNode basicData = (ObjectNode) mapper.readTree(basicDataFile);
+                applicationCreateForm.setBasicData(basicData);
+                application = client.application().create(applicationCreateForm);
+            }
+            packages = client.application().getPackages(application);
+
+            assertTrue(packages.length > 0);
+        } catch (IOException e) {
+            e.printStackTrace();
+            fail();
+        } catch (AcrosureException e) {
+            System.out.println(e.getMessage() + ", " + e.getStatusCode());
+            e.printStackTrace();
+            fail();
+        }
+    }
+
+    @Test
+    void SelectPackage_ByDefault_ReturnsApplicationWithPackage() {
+        try {
+            if (application.getStatus() != ApplicationStatus.PACKAGE_REQUIRED) {
+                ApplicationCreateForm applicationCreateForm = new ApplicationCreateForm();
+                applicationCreateForm.setProductId("prod_contractor");
+                ObjectNode basicData = (ObjectNode) mapper.readTree(basicDataFile);
+                applicationCreateForm.setBasicData(basicData);
+                application = client.application().create(applicationCreateForm);
+                packages = client.application().getPackages(application);
+            }
+
+            client.application().selectPackage(application, packages[0]);
+
+            assertAll("application",
+                    () -> assertEquals(application.getPackageCode(), packages[0].getPackageCode(),
+                            "Package code is not empty"),
+                    () -> assertNotNull(application.getPackageData(),
+                            "Package data is not null"),
+                    () -> assertSame(ApplicationStatus.DATA_REQUIRED, application.getStatus())
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
+            fail();
+        } catch (AcrosureException e) {
+            System.out.println(e.getMessage() + ", " + e.getStatusCode());
+            e.printStackTrace();
+            fail();
+        }
+    }
+
+    @Test
+    void Update_WithAdditionalData_AdvancesStatusToReady() {
+        try {
+            if (application.getStatus() != ApplicationStatus.DATA_REQUIRED) {
+                ApplicationCreateForm applicationCreateForm = new ApplicationCreateForm();
+                applicationCreateForm.setProductId("prod_contractor");
+                ObjectNode basicData = (ObjectNode) mapper.readTree(basicDataFile);
+                applicationCreateForm.setBasicData(basicData);
+                application = client.application().create(applicationCreateForm);
+                packages = client.application().getPackages(application);
+                client.application().selectPackage(application, packages[0]);
+            }
+
+            ObjectNode additionalData = (ObjectNode) mapper.readTree(additionalDataFile);
+            application.setAdditionalData(additionalData);
+            client.application().update(application);
+
+            assertSame(ApplicationStatus.READY, application.getStatus());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            fail();
+        } catch (AcrosureException e) {
+            System.out.println(e.getMessage() + ", " + e.getStatusCode());
+            e.printStackTrace();
+            fail();
+        }
+    }
+
+    @Test
+    void Submit_ByDefault_AdvancesStatusToSubmitted() {
+        try {
+            if (application.getStatus() != ApplicationStatus.READY) {
+                ApplicationCreateForm applicationCreateForm = new ApplicationCreateForm();
+                applicationCreateForm.setProductId("prod_contractor");
+                ObjectNode basicData = (ObjectNode) mapper.readTree(basicDataFile);
+                applicationCreateForm.setBasicData(basicData);
+                application = client.application().create(applicationCreateForm);
+                packages = client.application().getPackages(application);
+                client.application().selectPackage(application, packages[0]);
+                ObjectNode additionalData = (ObjectNode) mapper.readTree(additionalDataFile);
+                application.setAdditionalData(additionalData);
+                client.application().update(application);
+            }
+
+            client.application().submit(application);
+
+            assertSame(ApplicationStatus.SUBMITTED, application.getStatus());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            fail();
+        } catch (AcrosureException e) {
+            System.out.println(e.getMessage() + ", " + e.getStatusCode());
+            e.printStackTrace();
+            fail();
         }
     }
 }
