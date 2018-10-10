4560aa4b3224a204bbf77e8cc1491036e2bcfb11
Srikote Naewchampa <whcwhc78@gmail.com>
2018-10-10 20:18:23 +0700

    add File

---
+++ src/main/java/com/acrosure/resource/File.java
@@ -0,0 +1,32 @@
+package com.acrosure.resource;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class File {
+    private String title;
+    private String url;
+    private String signedUrl;
+
+    @JsonCreator
+    public File(
+            @JsonProperty("title") String title,
+            @JsonProperty("url") String url,
+            @JsonProperty("signed_url") String signedUrl) {
+        this.title = title;
+        this.url = url;
+        this.signedUrl = signedUrl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getSignedUrl() {
+        return signedUrl;
+    }
+}
