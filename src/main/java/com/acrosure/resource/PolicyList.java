25841b46e88bf4268198dc9a1c40f0bd1b18c5d8
Srikote Naewchampa <whcwhc78@gmail.com>
2018-10-10 19:05:46 +0700

    add PolicyList

---
+++ src/main/java/com/acrosure/resource/PolicyList.java
@@ -0,0 +1,35 @@
+package com.acrosure.resource;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Arrays;
+
+public class PolicyList {
+    private Application[] data;
+    private Pagination pagination;
+
+    @JsonCreator
+    public PolicyList(
+            @JsonProperty("data") Application[] data,
+            @JsonProperty("pagination") Pagination pagination) {
+        this.data = data;
+        this.pagination = pagination;
+    }
+
+    public Application[] getData() {
+        return data;
+    }
+
+    public Pagination getPagination() {
+        return pagination;
+    }
+
+    @Override
+    public String toString() {
+        return "ApplicationList{" +
+                "data=" + Arrays.toString(data) +
+                ", pagination=" + pagination +
+                '}';
+    }
+}
