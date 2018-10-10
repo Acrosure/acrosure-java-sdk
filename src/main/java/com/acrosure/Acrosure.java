e6467191d2430e3be2fb885ab2d475d54450035f
Srikote Naewchampa <whcwhc78@gmail.com>
2018-10-10 21:46:40 +0700

    add payment to acrosure

--- src/main/java/com/acrosure/Acrosure.java
+++ src/main/java/com/acrosure/Acrosure.java
@@ -2,6 +2,7 @@ package com.acrosure;
 
 public class Acrosure {
     private final ApplicationManager applicationManager;
+    private final Payment2C2PManager payment2C2PManager;
     private final ProductManager productManager;
     private final PolicyManager policyManager;
     private final TeamManager teamManager;
@@ -14,6 +15,7 @@ public class Acrosure {
     public Acrosure(String token, String host) {
         OkHttpClient client = new OkHttpClient(token, host);
         applicationManager = new ApplicationManager(client);
+        payment2C2PManager = new Payment2C2PManager(client);
         productManager = new ProductManager(client);
         policyManager = new PolicyManager(client);
         teamManager = new TeamManager(client);
@@ -22,6 +24,10 @@ public class Acrosure {
 
     public ApplicationManager application() {
         return applicationManager;
+    }
+
+    public Payment2C2PManager payment2C2P() {
+        return payment2C2PManager;
     }
 
     public ProductManager product() {
