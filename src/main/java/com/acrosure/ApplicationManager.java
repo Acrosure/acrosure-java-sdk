df51fbd9a1186f544c6c4330b09038c25d8a947d
Srikote Naewchampa <whcwhc78@gmail.com>
2018-10-11 15:00:13 +0700

    add comment

--- src/main/java/com/acrosure/ApplicationManager.java
+++ src/main/java/com/acrosure/ApplicationManager.java
@@ -15,6 +15,20 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 
+/**
+ * ApplicationManager handles all the application-specific requests.
+ * These includes:
+ * <ul>
+ *     <li>Get an existing application</li>
+ *     <li>List all the applications that associated with the token</li>
+ *     <li>Create an application</li>
+ *     <li>Update an existing application</li>
+ *     <li>Get insurance package(s) that is compatible with an application</li>
+ *     <li>Select insurance package for an application</li>
+ *     <li>Submit an application</li>
+ *     <li>Confirm an application</li>
+ * </ul>
+ */
 public class ApplicationManager {
     private final HttpClient httpClient;
     private final String METHOD_GROUP;
@@ -31,6 +45,14 @@ public class ApplicationManager {
         mapper.setDateFormat(df);
     }
 
+    /**
+     * Get an existing application
+     *
+     * @param applicationId         the application ID
+     * @return                      an application
+     * @throws IOException          if there are some JSON-related operation errors
+     * @throws AcrosureException    if the server returns error(s)
+     */
     public Application get(String applicationId) throws IOException, AcrosureException {
         ObjectNode requestPayload = mapper.createObjectNode();
 
@@ -40,6 +62,15 @@ public class ApplicationManager {
         return mapper.treeToValue(responseData.get("data"), Application.class);
     }
 
+    /**
+     * List all the applications that associated with the token
+     *
+     * @param query                 query information
+     * @return                      an instance of ApplicationList with
+     *                              pagination included
+     * @throws IOException          if there are some JSON-related operation errors
+     * @throws AcrosureException    if the server returns error(s)
+     */
     public ApplicationList list(ApplicationQuery query) throws IOException, AcrosureException {
         ObjectNode requestPayload = mapper.valueToTree(query);
 
@@ -48,6 +79,14 @@ public class ApplicationManager {
         return mapper.treeToValue(responseData, ApplicationList.class);
     }
 
+    /**
+     * Create an application
+     *
+     * @param productId             product ID, such as prod_motor etc.
+     * @return                      a created application
+     * @throws IOException          if there are some JSON-related operation errors
+     * @throws AcrosureException    if the server returns error(s)
+     */
     public Application create(String productId) throws IOException, AcrosureException {
         ApplicationCreateForm applicationCreateForm = new ApplicationCreateForm();
 
@@ -56,6 +95,14 @@ public class ApplicationManager {
         return this.create(applicationCreateForm);
     }
 
+    /**
+     * Create an application
+     *
+     * @param applicationCreateForm an instance of ApplicationCreateForm
+     * @return                      a created application
+     * @throws IOException          if there are some JSON-related operation errors
+     * @throws AcrosureException    if the server returns error(s)
+     */
     public Application create(ApplicationCreateForm applicationCreateForm) throws IOException, AcrosureException {
         ObjectNode requestPayload = mapper.valueToTree(applicationCreateForm);
         ObjectNode responseData = (ObjectNode) httpClient.call(METHOD_GROUP, Methods.CREATE.toString(), requestPayload);
@@ -63,6 +110,14 @@ public class ApplicationManager {
         return mapper.treeToValue(responseData.get("data"), Application.class);
     }
 
+    /**
+     * Update an existing application
+     *
+     * @param application           an application got from either #create or #get
+     * @return                      the updated application, points to the same instance as the argument
+     * @throws IOException          if there are some JSON-related operation errors
+     * @throws AcrosureException    if the server returns error(s)
+     */
     public Application update(Application application) throws IOException, AcrosureException {
         ApplicationUpdateForm applicationUpdateForm = mapper.convertValue(application, ApplicationUpdateForm.class);
         ObjectNode requestPayload = mapper.valueToTree(applicationUpdateForm);
@@ -73,6 +128,14 @@ public class ApplicationManager {
         return application.copy(origin);
     }
 
+    /**
+     * Get an insurance package the application selected
+     *
+     * @param application           an instance of resource.Application
+     * @return                      an instance of resource.Package
+     * @throws IOException          if there are some JSON-related operation errors
+     * @throws AcrosureException    if the server returns error(s)
+     */
     public Package getPackage(Application application) throws IOException, AcrosureException {
         ObjectNode requestPayload = mapper.createObjectNode();
 
@@ -82,6 +145,14 @@ public class ApplicationManager {
         return mapper.treeToValue(responseData.get("data"), Package.class);
     }
 
+    /**
+     * Get insurance packages which are compatible with the application
+     *
+     * @param application           an instance of resource.Application
+     * @return                      array of resource.Package
+     * @throws IOException          if there are some JSON-related operation errors
+     * @throws AcrosureException    if the server returns error(s)
+     */
     public Package[] getPackages(Application application) throws IOException, AcrosureException {
         ObjectNode requestPayload = mapper.createObjectNode();
 
@@ -91,6 +162,15 @@ public class ApplicationManager {
         return mapper.treeToValue(responseData.get("data"), Package[].class);
     }
 
+    /**
+     * Select insurance package for an application
+     *
+     * @param application           an instance of resource.Application
+     * @param mPackage              an instance of resource.Package got from #getPackages
+     * @return                      the same application with an insurance package associated
+     * @throws IOException          if there are some JSON-related operation errors
+     * @throws AcrosureException    if the server returns error(s)
+     */
     public Application selectPackage(Application application, Package mPackage) throws  IOException, AcrosureException {
         ObjectNode requestPayload = mapper.createObjectNode();
 
@@ -103,6 +183,14 @@ public class ApplicationManager {
         return application.copy(origin);
     }
 
+    /**
+     * Submit an application
+     *
+     * @param application           an instance of resource.Application
+     * @return                      the submitted application
+     * @throws IOException          if there are some JSON-related operation errors
+     * @throws AcrosureException    if the server returns error(s)
+     */
     public Application submit(Application application) throws IOException, AcrosureException {
         ObjectNode requestPayload = mapper.createObjectNode();
 
@@ -114,6 +202,14 @@ public class ApplicationManager {
         return application.copy(origin);
     }
 
+    /**
+     * Confirm an application
+     *
+     * @param application           an instance of resource.Application
+     * @return                      the confirmed application
+     * @throws IOException          if there are some JSON-related operation errors
+     * @throws AcrosureException    if the server returns error(s)
+     */
     public Policy[] confirm(Application application) throws IOException, AcrosureException {
         ObjectNode requestPayload = (ObjectNode) mapper.readTree("{}");
 
