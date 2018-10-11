a7e356f04dd5a037b283d0d9656fdecaca6834e0
Srikote Naewchampa <whcwhc78@gmail.com>
2018-10-11 15:11:35 +0700

    add comment

--- src/main/java/com/acrosure/HttpClient.java
+++ src/main/java/com/acrosure/HttpClient.java
@@ -5,6 +5,21 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import java.io.IOException;
 
+/**
+ * HttpClient defines the interface that any HTTP clients need to implement.
+ * In order the replace the current client, users can create their own by implementing
+ * this interface.
+ */
 interface HttpClient {
+    /**
+     * Makes an HTTP request to the server
+     *
+     * @param methodGroup           the method group
+     * @param method                the method
+     * @param param                 the request body resides in POST message
+     * @return                      an instance of JsonNode
+     * @throws IOException          if there are some JSON-related operation errors
+     * @throws AcrosureException    if the server returns error(s)
+     */
     JsonNode call(String methodGroup, String method, ObjectNode param) throws IOException, AcrosureException;
 }
